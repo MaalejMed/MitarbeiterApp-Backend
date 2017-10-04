9a91e8c90358b07c86eafbbdf2632539c6699f1b
mmaalej <Mohamed.Maalej@cognizant.com>
2017-10-04 10:06:54 +0200

    Fix issue in Login service, avoid holding same result for all REST Calls

--- Sources/OnBoarding_Backend/Server/main.swift
+++ Sources/OnBoarding_Backend/Server/main.swift
@@ -8,11 +8,9 @@ let router = Router()
 let connection = MySQLConnection(host: "localhost", user: "root", password: "mysql", database: "cognizant",
                                  port: 3306, characterSet: nil)
 
+//Authentication services
 let loginMS = Login(router: router, connection: connection)
 
-// Add an HTTP server and connect it to the router
 Kitura.addHTTPServer(onPort: 8080, with: router)
-
-// Start the Kitura runloop (this call never returns)
 Kitura.run()
     
