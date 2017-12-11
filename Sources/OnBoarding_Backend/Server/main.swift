6f049b4ff339bc8c138e6a478e506517fd033702
mmaalej <Mohamed.Maalej@cognizant.com>
2017-12-11 15:33:35 +0100

    Add connection object to each service to ovoid mysql conflicts when triggering sql queries in //

--- Sources/OnBoarding_Backend/Server/main.swift
+++ Sources/OnBoarding_Backend/Server/main.swift
@@ -5,20 +5,27 @@ import SwiftKuery
 //MARK:- Create a new router
 let router = Router()
 
-let connection = MySQLConnection(host: "localhost", user: "root", password: "mysql", database: "cognizant",
-                                 port: 3306, characterSet: nil)
+
 
 //MARK:- Associate services
-let associateService = AssociateService(router: router, connection: connection)
+let asConnection = MySQLConnection(host: "localhost", user: "root", password: "mysql", database: "cognizant",
+                                 port: 3306, characterSet: nil)
+let associateService = AssociateService(router: router, connection: asConnection)
 
 //MARK:- Feed services
-let feedService = FeedService(router: router, connection: connection)
+let fsConnection = MySQLConnection(host: "localhost", user: "root", password: "mysql", database: "cognizant",
+                                 port: 3306, characterSet: nil)
+let feedService = FeedService(router: router, connection: fsConnection)
 
 //MARK:- Time services
-let timeService = TimeService(router: router, connection: connection)
+let tsConnection = MySQLConnection(host: "localhost", user: "root", password: "mysql", database: "cognizant",
+                                 port: 3306, characterSet: nil)
+let timeService = TimeService(router: router, connection: tsConnection)
 
 //MARK: - Message services
-let messageService = MessageService(router: router, connection: connection)
+let msConnection = MySQLConnection(host: "localhost", user: "root", password: "mysql", database: "cognizant",
+                                   port: 3306, characterSet: nil)
+let messageService = MessageService(router: router, connection: msConnection)
 
 //MARK:-Start Service
 Kitura.addHTTPServer(onPort: 8080, with: router)
